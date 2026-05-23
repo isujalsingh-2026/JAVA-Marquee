afc13577d433e3aef41a0bd75ebbb86f9fa2aba9
isujalsingh-2026 <isujalsingh2026@gmail.com>
2026-05-23 17:17:34 +0530

    Fix: remove submodules and convert to normal folders

---
+++ Pattern/addition.java
@@ -0,0 +1,11 @@
+package Marquee.JavaFunction;
+
+public class addition {
+    public static void add(int a, int b) {
+        System.out.println(a+b);
+    }
+   public static void main(String[] args) {
+        add(5,10);
+
+    }
+}
